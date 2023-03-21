2da7a8ae15620a804f462fad7685b0b1d4d17bb7
Andreas Agerblad <aagerblad@spotify.com>
2023-03-21 15:15:20 -0400

    Fix build issue

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-import Link from 'next/link'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+import Link from "next/link";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
@@ -16,11 +16,10 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <h1 className='title'>
-          Hello hello <a href='/posts/first-post'>link</a>
+        <h1 className="title">
+          Hello hello <Link href="/posts/first-post">link</Link>
         </h1>
-
       </main>
     </>
-  )
+  );
 }
