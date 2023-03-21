a92584f4c8bfb7452c3266b004f5909dc5fe579e
Andreas Agerblad <aagerblad@spotify.com>
2023-03-21 15:12:44 -0400

    Add maps view

---
+++ pages/posts/first-post.tsx
@@ -0,0 +1,43 @@
+import { useLoadScript, GoogleMap } from "@react-google-maps/api";
+import type { NextPage } from "next";
+import Link from "next/link";
+import { useMemo } from "react";
+
+export default function FirstPost() {
+  const libraries = useMemo(() => ["places"], []);
+
+  const mapCenter = useMemo(
+    () => ({ lat: 40.691337426702155, lng: -73.98855438877749 }),
+    []
+  );
+  const mapOptions = useMemo<google.maps.MapOptions>(
+    () => ({
+      disableDefaultUI: true,
+      clickableIcons: true,
+      scrollwheel: false,
+    }),
+    []
+  );
+
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_KEY as string,
+    libraries: libraries as any,
+  });
+
+  if (!isLoaded) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <>
+      <GoogleMap
+        options={mapOptions}
+        zoom={14}
+        center={mapCenter}
+        mapTypeId={google.maps.MapTypeId.ROADMAP}
+        mapContainerStyle={{ width: "800px", height: "800px" }}
+        onLoad={() => console.log("Map Component Loaded...")}
+      />
+    </>
+  );
+}
